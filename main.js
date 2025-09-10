a0dbd3e8c38afb4063b7b398a39d91efe2701e2a
ReimboyevShohrux <shohruxreimboyev4@gmail.com>
2025-09-10 18:26:50 +0300

    project start + js

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// let son = +prompt("Son kiriting");
+
+// if ( son >= 0) {
+//     alert("musbat son")
+// }else {
+//     alert("manfiy son")
+// }
+
+// let raqam = +prompt("Son kiriting");
+
+// if ( raqam >= 0) {
+//     alert("musbat son")
+// }else {
+//     alert("manfiy son")
+// }
+
+
+// let yosh = +prompt("Iltimos yoshingizni kiriting");
+
+// if ( yosh >= 21) {
+//     alert("siz ulg'aygansiz")
+// }else {
+//     alert("siz hali ulg'aymagansiz")
+// }
+
+// let лет = +prompt("Iltimos yoshingizni kiriting");
+
+// if ( лет >= 21) {
+//     alert("siz ulg'aygansiz")
+// }else {
+//     alert("siz hali ulg'aymagansiz")
+// }
