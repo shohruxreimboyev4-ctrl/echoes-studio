3e0852d8051c3d920f8df21c08ff0bad635462ec
ReimboyevShohrux <shohruxreimboyev4@gmail.com>
2025-09-10 19:02:25 +0300

    js matter done!

--- main.js
+++ main.js
@@ -1,33 +1,74 @@
 
-// let son = +prompt("Son kiriting");
+let son = +prompt("Son kiriting");
 
-// if ( son >= 0) {
-//     alert("musbat son")
-// }else {
-//     alert("manfiy son")
-// }
+if ( son >= 0) {
+    alert("musbat son")
+}else {
+    alert("manfiy son")
+}
 
-// let raqam = +prompt("Son kiriting");
+let raqam = +prompt("Son kiriting");
 
-// if ( raqam >= 0) {
-//     alert("musbat son")
-// }else {
-//     alert("manfiy son")
-// }
+if ( raqam >= 0) {
+    alert("musbat son")
+}else {
+    alert("manfiy son")
+}
 
 
-// let yosh = +prompt("Iltimos yoshingizni kiriting");
+let yosh = +prompt("Iltimos yoshingizni kiriting");
 
-// if ( yosh >= 21) {
-//     alert("siz ulg'aygansiz")
-// }else {
-//     alert("siz hali ulg'aymagansiz")
-// }
+if ( yosh >= 21) {
+    alert("siz ulg'aygansiz")
+}else {
+    alert("siz hali ulg'aymagansiz")
+}
 
-// let лет = +prompt("Iltimos yoshingizni kiriting");
+let лет = +prompt("Iltimos yoshingizni kiriting");
 
-// if ( лет >= 21) {
-//     alert("siz ulg'aygansiz")
-// }else {
-//     alert("siz hali ulg'aymagansiz")
-// }
+if ( лет >= 21) {
+    alert("siz ulg'aygansiz")
+}else {
+    alert("siz hali ulg'aymagansiz")
+}
+
+let week = +prompt("hafta kunini kiriting");
+
+if ( week == 1) {
+    alert("dushanba")
+}else if (week == 2) {
+    alert("seshanba")
+}else if (week == 3) {
+    alert("chorshanba")
+}else if (week == 4) {
+    alert("payshanba")
+}else if (week == 5) {
+    alert("juma")
+}else if (week == 6) {
+    alert("shanba")
+}else if (week == 7) {
+    alert("yakshanba")
+}else {
+    alert("bunday hafta kuni yo'q")
+}
+
+
+let temperature = +prompt("iltimos haroratni kiriting");
+
+if ( temperature >= 30) {
+    alert("harorat juda yuqori")
+}else {
+    alert("harorat normal")
+}
+
+
+let n = prompt("Son kiriting:");
+
+n = Number(n);
+
+if (n % 2 === 0) {  
+  alert(n * 3);
+} else {  
+  alert(n * 2);
+}
+
